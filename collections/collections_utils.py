43e9d4c0aa34f72de87e029d4c5803c9ac4a94ac
Juraj Dubrava <jdubrava15@gmail.com>
2021-12-25 18:14:47 +0100

    Collections: more examples

--- collections/collections_utils.py
+++ collections/collections_utils.py
@@ -1,26 +1,32 @@
 import logging
-from collections import namedtuple
+from collections import namedtuple, ChainMap, Counter
+from typing import Iterable, Mapping, Union
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic, great_circle
-# collections datatypes 
-# geographic point holding latitude and longtitude values 
-GeoPoint = namedtuple("GeoPoint", ['lat','long'])
-# amino acid holding abbreviation of the amino acid and its full name 
-AminoAcid = namedtuple("AminoAcid", ['abbr', 'full_name'])
+
+# collections datatypes
+# geographic point holding latitude and longtitude values
+GeoPoint = namedtuple("GeoPoint", ["lat", "long"])
+# amino acid holding abbreviation of the amino acid and its full name
+AminoAcid = namedtuple("AminoAcid", ["abbr", "full_name"])
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 locator = Nominatim(user_agent="geo_app")
 
+
 def get_address(geo_point: GeoPoint) -> str:
     """Get address of geographical point"""
 
     point = f"{geo_point.lat}, {geo_point.long}"
     location = locator.reverse(point)
-    logger.info(f"Address of point ({geo_point.lat},{geo_point.long}) is: {location.address}")
+    logger.info(
+        f"Address of point ({geo_point.lat},{geo_point.long}) is: {location.address}"
+    )
 
-    return location.address 
+    return location.address
+
 
 def get_distance(point1: GeoPoint, point2: GeoPoint, units="km") -> float:
     """Get distance between two geographical points"""
@@ -33,7 +39,29 @@ def get_distance(point1: GeoPoint, point2: GeoPoint, units="km") -> float:
         distance = distance.km
     else:
         distance = distance.miles
-    
-    print(f"Distance between {address1} and {address2} is {format(distance, '.2f')} {units}")
+
+    print(
+        f"Distance between {address1} and {address2} is {format(distance, '.2f')} {units}"
+    )
 
     return distance
+
+
+# ChainMap example
+def create_mapping(*mappings):
+    mapping = ChainMap(mappings)
+    return mapping
+
+
+def chain_map_to_dict(chain_map):
+    full_map = {}
+    (maps,) = chain_map.maps
+    for mapping in maps:
+        full_map.update(mapping)
+    return full_map
+
+
+# Counter example
+def most_common(value: Union[Iterable, Mapping], limit=None):
+    common_values = Counter(value).most_common(limit)
+    return common_values
--- collections/main.py
+++ collections/main.py
@@ -1,17 +1,62 @@
 import csv
 import pathlib
 from itertools import combinations
-from collections_utils import GeoPoint,get_distance
+from collections_utils import (
+    GeoPoint,
+    get_distance,
+    create_mapping,
+    chain_map_to_dict,
+    most_common,
+)
 
 if __name__ == "__main__":
 
     filepath = pathlib.Path(__file__).parent.joinpath("points.csv")
     # read geo points from csv file
-    with open(filepath, 'r') as points_file:
+    with open(filepath, "r") as points_file:
         locations = map(GeoPoint._make, csv.reader(points_file))
-    
+
         # get all combinations of points in csv
-        locations_pairs = list(combinations(locations,2))
+        locations_pairs = list(combinations(locations, 2))
 
         for location_pair in locations_pairs:
             distance = get_distance(location_pair[0], location_pair[1])
+
+    # chain map example
+    base_mapping = {
+        "interpret": "Linkin Park",
+        "city": "Aguora Hills",
+        "state": "California",
+    }
+    genres_mapping = {
+        "genres": [
+            "alternative rock",
+            "nu-metal",
+            "rap rock",
+            "electronic rock",
+            "pop rock",
+        ]
+    }
+    discography_mapping = {
+        "albums": [
+            ("Hibrid Theory", "2000"),
+            ("Meteora", "2003"),
+            ("Minutes to Midnight", "2007"),
+            ("A Thousand Suns", "2010"),
+            ("Living Things", "2012"),
+            ("The Hunting Party", "2014"),
+            ("One More Light", "2017"),
+        ]
+    }
+
+    full_info = create_mapping(base_mapping, genres_mapping, discography_mapping)
+    full_map = chain_map_to_dict(full_info)
+    print(full_map)
+
+    # counter example
+
+    # insuline amino acid sequence
+    PROTEIN = "MALWMRLLPLLALLALWGPDPAAAFVNQHLCGSHLVEALYLVCGERGFFYTPKTRREAEDLQVGQVELGGGPGAGSL\
+    QPLALEGSLQKRGIVEQCCTSICSLYQLENYCN"
+    # get 5 most common amino acids in the sequence
+    most_common_amino_acids = most_common(PROTEIN, 5)
